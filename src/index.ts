5129b82ba40192ec9615a8588f867d1737b8d0f4
Daniel Hermon <danielhermonsync@gmail.com>
2021-01-20 11:12:55 +0200

    feat(error-handler): used named variable for function infavor of opentelemetry tracing (#2)
    * feat(error-handler): used named variable for function infavor of opentelemetry tracing
    
    * style: format
    
    * refactor(error-handler): extracted log type to a module type instead of inline

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
 import { NextFunction, ErrorRequestHandler } from 'express';
-import { StatusCodes, getReasonPhrase } from "http-status-codes";
+import { StatusCodes, getReasonPhrase } from 'http-status-codes';
+
+type LogFunction  = (message: string) => void;
 export interface HttpError extends Error {
   statusCode?: StatusCodes;
   status?: StatusCodes;
-};
+}
 
 export interface ErrorResponse {
   message: string;
   stacktrace?: string;
-};
+}
 
-export const getErrorHandlerMiddleware: (log: (message: string) => void) => ErrorRequestHandler = (log) =>
-   (
+export const getErrorHandlerMiddleware: (log: LogFunction) => ErrorRequestHandler = (log) => {
+  const mapColoniesErrorExpressHandler: ErrorRequestHandler = (
     err: HttpError,
     req,
     res,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     next: NextFunction
-    ): void => {
+  ): void => {
     log(`${req.method} request to ${req.originalUrl}  has failed with error: ${err.message}`);
     const errorResponse: ErrorResponse = {
-      message: err.message
+      message: err.message,
     };
     const responseStatusCode = err.statusCode ?? err.status ?? StatusCodes.INTERNAL_SERVER_ERROR;
 
-    if(responseStatusCode >= StatusCodes.INTERNAL_SERVER_ERROR) {
+    if (responseStatusCode >= StatusCodes.INTERNAL_SERVER_ERROR) {
       if (process.env.NODE_ENV === 'production') {
         errorResponse.message = getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR);
       } else {
@@ -33,3 +35,6 @@ export const getErrorHandlerMiddleware: (log: (message: string) => void) => Erro
     }
     res.status(responseStatusCode).json(errorResponse);
   };
+
+  return mapColoniesErrorExpressHandler;
+};
